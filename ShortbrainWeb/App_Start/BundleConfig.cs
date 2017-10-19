92d625813ce210462b74694d781858c833c3940c
Cyril Rat <cyril.rat@gmail.com>
2017-10-19 09:24:21 +0200

    Added theme bootstrap slate

--- ShortbrainWeb/App_Start/BundleConfig.cs
+++ ShortbrainWeb/App_Start/BundleConfig.cs
@@ -20,11 +20,11 @@ namespace ShortbrainWeb
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap-slate.js",
+                      "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-slate.css",
                       "~/Content/site.css"));
         }
     }
