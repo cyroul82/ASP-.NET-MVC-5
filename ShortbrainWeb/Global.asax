677a8f89bdb191129a45921c2d879b4a478a0628
Cyril Rat <cyril.rat@gmail.com>
2017-10-19 08:57:05 +0200

    Ajoutez des fichiers projet.

---
+++ ShortbrainWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShortbrainWeb.MvcApplication" Language="C#" %>
---
+++ ShortbrainWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShortbrainWeb.Startup))]
+namespace ShortbrainWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
