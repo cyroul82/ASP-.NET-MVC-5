212a9aa26df6604bd629557553518dc553bcc751
Cyril Rat <cyril.rat@gmail.com>
2017-10-19 16:18:04 +0200

    Added view project and added the list of project

--- ShortbrainWeb/Controllers/ProjectsController.cs
+++ ShortbrainWeb/Controllers/ProjectsController.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShortbrainWeb.Models;
 
 namespace ShortbrainWeb.Controllers
 {
     public class ProjectsController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public ProjectsController()
+        {
+            _context = new ApplicationDbContext();
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Projects
         public ActionResult Index()
         {
-            return View();
+            var projects = _context.Projects.Include(p => p.Category).ToList();
+
+            return View(projects);
         }
     }
 }
